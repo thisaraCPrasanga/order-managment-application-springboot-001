5fb4c2abca151e5ed6bb2a47a4534537f57461ae
Thisara C Prasanga <thisaraprasanga.dev@gmail.com>
2026-03-20 22:09:10 +0530

    Make model Mappper

--- src/main/java/com/possystem/pos/PosApplication.java
+++ src/main/java/com/possystem/pos/PosApplication.java
@@ -1,13 +1,20 @@
 package com.possystem.pos;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PosApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
